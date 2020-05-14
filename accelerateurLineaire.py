d388b3069d516f156237749e34670d0ecc395cc9
Guillaume Philippe <gphilippe@juniorisep.com>
2020-05-14 19:42:46 +0200

    new

--- accelerateurLineaire.py
+++ accelerateurLineaire.py
@@ -10,8 +10,17 @@ from matplotlib.figure import Figure
 def onCLick():
     return
 
-def accelerateur_lineaire(c,v0,a0,duree_vie_propre):
-    x = np.linspace(0,10,100000)
+def accelerateur_lineaire():
+    global c
+    global vitesseIniValue
+    global accelerationValue
+    global tPropreValue
+
+    v0 = vitesseIniValue.get()
+    a0 = accelerationValue.get()
+    duree_vie_propre = tPropreValue.get()
+
+    x = np.linspace(0,10,100)
     a_array = []
     v_array = []
     tempsPropre = []
@@ -20,47 +29,51 @@ def accelerateur_lineaire(c,v0,a0,duree_vie_propre):
         a = acceleration(elt,a0)
         a_array.append(a)
         v_array.append(vitesse(elt,v0))
-        tempsPropre.append(temps_propre(elt,a))
+        tempsPropre.append(temps_propre(elt,a,c))
     
-    plt.figure(0)
-    plt.subplot(211)
-    plt.plot(x,a_array)
-    plt.xlabel('temps (s)')
-    plt.ylabel('acceleration (m/s2)')
-    plt.subplot(212)
-    plt.plot(x,v_array)
-    plt.xlabel('temps (s)')
-    plt.ylabel('vitesse (m/s)')
+    fig1 = Figure()
+    ax1 = fig1.add_subplot(211, xlabel='temps (s)', ylabel='acceleration (m/s2)' )
+    ax1.plot(x,a_array)
+    ax1.grid(True)
+    ax2 = fig1.add_subplot(212, xlabel='temps (s)', ylabel='vitesse (m/s)')
+    ax2.plot(x,v_array)
+    ax2.grid(True)
 
     fig = Figure()
     ax = fig.add_subplot(111, xlabel='v0 (m/s2)', ylabel='espérance de vie (s)')
-    ax.plot(x,a_array)
+    ax.plot(x,v_array)
     ax.grid(True)
 
     graph = FigureCanvasTkAgg(fig, master=win)
     canvas = graph.get_tk_widget()
-    canvas.grid(row=0, column=0, pady=0, columnspan = 4)
+    canvas.grid(row=0, column=0, pady=0, columnspan = 5)
+
+    graph2 = FigureCanvasTkAgg(fig1, master=win)
+    canvas2 = graph2.get_tk_widget()
+    canvas2.grid(row=0, column=6)
+
+
 
 
 #def vitesse(a,t,v0,c):
 #    return (a*t)/(1+(a*t/c)**2)**(1/2)
 
-def temps_propre(t,a):
+def temps_propre(t,a,c):
     return (c/a)*acos(a*t/c)
 
 #def position(c,a,t):
 #    return (c**2/a)*(1+((a*t)/c)**2)**(1/2)
 
 def acceleration(t,a):
-    return a*exp(-t)
+    print(exp(-t))
+    return a*exp(-t)+5e-324
 
 def vitesse(t,v0):
     return (c-v0)*(1-exp(-t))+v0
 
-g = 9.81
 c = 299_792_458
 v0 = 0.9*c
-a = g
+a = 9.81
 duree_vie_propre = 2.2E-6
 t=0
 
@@ -69,43 +82,54 @@ t=0
 win = Tk()
 win.title("Acceleration constante le long d'un axe")
 
-celeriteVar = StringVar()
 vitesseIniVar = StringVar()
 accelerationVar = StringVar()
 tPropreVar = StringVar()
 
-celeriteVar.set("Célérité")
+vitesseIniValue = DoubleVar()
+accelerationValue = DoubleVar()
+tPropreValue = DoubleVar()
+
+vitesseIniValue.set(v0)
+accelerationValue.set(a)
+tPropreValue.set(duree_vie_propre)
+
 vitesseIniVar.set("Vitesse initial")
 accelerationVar.set("Accélération")
 tPropreVar.set("Temps propre")
 
-celerite_text = Label(win, text = celeriteVar.get())
 vitesseIni_text = Label(win, text = vitesseIniVar.get())
 acceleration_text = Label(win, text = accelerationVar.get())
 tPropre_text = Label(win, text = tPropreVar.get())
 
-celerite_input = Entry(win)
-vitesseIni_input = Entry(win)
-acceleration_input = Entry(win)
-tPropre_input = Entry(win)
+vitesseIni_input = Entry(win, textvariable = vitesseIniValue)
+acceleration_input = Entry(win, textvariable = accelerationValue)
+tPropre_input = Entry(win, textvariable = tPropreValue)
 
-calcul = Button(win, text='Calculer')
+calcul = Button(win, text='Calculer', command=accelerateur_lineaire)
 fig = Figure()
 ax = fig.add_subplot(111)
 ax.grid(True)
 
+fig1 = Figure()
+ax1 = fig1.add_subplot(211, xlabel='temps (s)', ylabel='acceleration (m/s2)' )
+ax1.grid(True)
+ax2 = fig1.add_subplot(212, xlabel='temps (s)', ylabel='vitesse (m/s)')
+ax2.grid(True)
+
 graph = FigureCanvasTkAgg(fig, master=win)
+graph2 = FigureCanvasTkAgg(fig1, master=win)
 canvas = graph.get_tk_widget()
-canvas.grid(row=0, column=0, pady=0, columnspan = 4)
-celerite_text.grid(row=1, column=0)
-vitesseIni_text.grid(row=1, column=1)
-acceleration_text.grid(row=1, column=2)
-tPropre_text.grid(row=1, column=3)
-celerite_input.grid(row=2, column=0)
-vitesseIni_input.grid(row=2, column=1)
-acceleration_input.grid(row=2, column=2)
-tPropre_input.grid(row=2, column=3)
-calcul.grid(row = 3, column = 0, pady=0, columnspan = 4)
+canvas2 = graph2.get_tk_widget()
+canvas.grid(row=0, column=0, pady=0, columnspan = 5)
+canvas2.grid(row=0, column=6)
+vitesseIni_text.grid(row=1, column=0)
+acceleration_text.grid(row=1, column=1)
+tPropre_text.grid(row=1, column=2)
+vitesseIni_input.grid(row=2, column=0)
+acceleration_input.grid(row=2, column=1)
+tPropre_input.grid(row=2, column=2)
+calcul.grid(row = 1, column = 3, pady=0, rowspan = 2, columnspan = 2)
 win.mainloop()
 
 
