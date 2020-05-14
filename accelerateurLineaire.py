9cadbe665e2ae01b77f92e89a86442cfba2b0a08
Guillaume Philippe <gphilippe@juniorisep.com>
2020-05-14 12:24:29 +0200

    add esperance de vie

--- accelerateurLineaire.py
+++ accelerateurLineaire.py
@@ -2,15 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import acos,exp
 
-def accelerateur_lineaire(c,v0,a,duree_vie_propre):
+def accelerateur_lineaire(c,v0,a0,duree_vie_propre):
     x = np.linspace(0,10,100000)
     a_array = []
     v_array = []
+    tempsPropre = []
 
     for elt in x:
-        a_array.append(acceleration(elt,a))
+        a = acceleration(elt,a0)
+        a_array.append(a)
         v_array.append(vitesse(elt,v0))
+        tempsPropre.append(temps_propre(elt,a))
     
+    plt.figure(0)
     plt.subplot(211)
     plt.plot(x,a_array)
     plt.xlabel('temps (s)')
@@ -19,17 +23,26 @@ def accelerateur_lineaire(c,v0,a,duree_vie_propre):
     plt.plot(x,v_array)
     plt.xlabel('temps (s)')
     plt.ylabel('vitesse (m/s)')
+
+    plt.figure(1)
+    plt.plot(x,a_array)
+    plt.xlabel('v0 (m/s2)')
+    plt.ylabel('espérance de vie (s)')
     plt.show()
+
+
+
+
     return 
 
 #def vitesse(a,t,v0,c):
 #    return (a*t)/(1+(a*t/c)**2)**(1/2)
 
-def temps_propre(c,a,t):
+def temps_propre(t,a):
     return (c/a)*acos(a*t/c)
 
-def position(c,a,t):
-    return (c**2/a)*(1+((a*t)/c)**2)**(1/2)
+#def position(c,a,t):
+#    return (c**2/a)*(1+((a*t)/c)**2)**(1/2)
 
 def acceleration(t,a):
     return a*exp(-t)
@@ -45,6 +58,5 @@ duree_vie_propre = 2.2E-6
 t=0
 
 accelerateur_lineaire(c,v0,a,duree_vie_propre)
-#print(vitesse(a,1,v0,c))
 
 
