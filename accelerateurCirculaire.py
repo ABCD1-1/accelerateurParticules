84a18df07e4816f53028efa8b5cafc10fcbabd9b
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2020-05-17 23:12:51 +0200

    17/05 quasi ok

--- accelerateurCirculaire.py
+++ accelerateurCirculaire.py
@@ -36,71 +36,78 @@ from matplotlib.figure import Figure
 from math import pi, sqrt
 
 """
-Variables
+Variables d'initialisation
 """
 c = 299_792_458  # c = constante de célérité de la lumière dans le vide en m/s
 # r = int(input("Saisissez le rayon du cercle (en mètre): "))
-rayon = 6400  # doit etre modifiable
-# v0 = float(input("Saisissez le rapport vitesse du muon / celerite de la lumière :\n(exemple 0.9 pour une vitesse de 0.9*c) "))*c
-v0 = 0.9 * c
-# dureeViePropre = float(input("Saisissez la durée de vie d'un muon en microseconde :\n(exemple 6 pour une durée de de 6 µs"))
-dureeViePropre = 2.2E-6  # doit etre modifiable
+rayon = 6400
+
+vitesseTanSur_c_input = 0.9
+v_tan_input = vitesseTanSur_c_input * c
+
+
+dureeViePropre = 2.2E-6
 v_t = np.linspace(0, 1, 1000)
 dureeVieLaboTableau = []
 distanceMoyenneTableau = []
 
-for i in range(len(v_t)):
-    vitesse = v_t[i] * c - 0.000001  # -0.000001 pour éviter l'erreur de division par 0
-    gamma = 1 / sqrt(1 - vitesse ** 2 / (c ** 2))
-    v_angulaire = vitesse / rayon
-    a_n = -vitesse ** 2 / rayon
-    dureeVieMoyenneLabo = dureeViePropre * gamma  # à déterminer
-    distanceMoyenne = vitesse * dureeVieMoyenneLabo  # à déterminer
-    dureeVieLaboTableau.append(dureeVieMoyenneLabo)
-    distanceMoyenneTableau.append(distanceMoyenne)
-    # print("Durée de vie en labo (en secondes) : ",dureeVieMoyenneLabo)
-    # print("\nDistance moyenne parcourue en mètre : ",distanceMoyenne)
-
-print(len(dureeVieLaboTableau))
 
 
-# print(dureeVieLaboTableau)
-# print(distanceMoyenneTableau)
-
-
-def accelerateur_lineaire():
+def lancement_calcul():
     global c
-    global vitesseIniValue
+    global vitesseTanSur_cValue
+    v_tan_input = vitesseTanSur_cValue.get()*c
+
+    # v_tan_input = 0.9*c
     global rayonValue
     global dureeViePropreValue
 
-    v0 = vitesseIniValue.get()
-    a0 = rayonValue.get()
-    duree_vie_propre = dureeViePropreValue.get()
+    # a0 = rayonValue.get()
+    # duree_vie_propre = dureeViePropreValue.get()
 
-    x = np.linspace(0, 10, 100)
-    a_array = []
-    v_array = []
-    tempsPropre = []
+    v_t = np.linspace(0, 0.999, 1000)           # 0.999 et pas 1 pour avoir un graphique lisible
 
-    # for elt in x:
-    # a = acceleration(elt, a0)
-    # a_array.append(a)
-    # v_array.append(vitesse(elt, v0))
-    # tempsPropre.append(temps_propre(elt, a, c))
+    dureeVieLaboTableau = []
+    distanceMoyenneTableau = []
+    v_angulaireTableau = []
 
-    fig1 = Figure()
-    ax1 = fig1.add_subplot(211, xlabel='temps (s)', ylabel='acceleration (m/s2)')
-    ax1.plot(x, a_array)
-    ax1.grid(True)
-    ax2 = fig1.add_subplot(212, xlabel='temps (s)', ylabel='vitesse (m/s)')
-    ax2.plot(x, v_array)
-    ax2.grid(True)
+    for element in v_t:
+        vitesse = element * c
+        gamma = 1 / sqrt(1 - vitesse ** 2 / (c ** 2))
+        v_angulaire = (vitesse / (rayonValue.get()*2*pi))     # vitesse angulaire en tours / s
+        dureeVieMoyenneLabo = dureeViePropreValue.get() * gamma
+        distanceMoyenne = vitesse * dureeVieMoyenneLabo
+        dureeVieLaboTableau.append(dureeVieMoyenneLabo)
+        distanceMoyenneTableau.append(distanceMoyenne)
+        v_angulaireTableau.append(v_angulaire)
+
+    # calcul en fonction de l'entrée de l'utilisateur
+    gamma_v_input_case = 1 / sqrt(1 - v_tan_input ** 2 / (c ** 2))
+    dureeVie_v_input_case = dureeViePropreValue.get() * gamma_v_input_case
+    v_angulaire_input_case = (v_tan_input / (rayonValue.get() * 2 * pi))
+    distanceMoyenne_input_case = v_tan_input * dureeVie_v_input_case
+
 
     fig = Figure()
-    ax = fig.add_subplot(111, xlabel='v0 (m/s2)', ylabel='espérance de vie (s)')
-    ax.plot(x, v_array)
+    ax = fig.add_subplot(111, xlabel='Rapport vitesse tangentielle sur c : v_t/c',
+                         ylabel='Durée de vie du muon en laboratoire (s)')
+    ax.plot(v_t, dureeVieLaboTableau)
+    print(v_tan_input / c)
+    print(dureeVie_v_input_case)
+    ax.plot(v_tan_input / c, dureeVie_v_input_case, marker="v", color="red")
     ax.grid(True)
+    duree_vie_text = Label(win, text="Durée de vie estimée en laboratoire (s) : " + dureeVie_v_input_case.__str__())                       # afficher la durée de vie estimée
+    duree_vie_text.grid(row=3, column=1)
+    print(v_angulaire_input_case)
+    print(distanceMoyenne_input_case)
+    fig1 = Figure()
+    ax1 = fig1.add_subplot(111, xlabel='Période de rotation (tours/s)', ylabel='Distance moyenne parcourue (en m)')
+    ax1.plot(v_angulaireTableau, distanceMoyenneTableau)
+    ax1.plot(v_angulaire_input_case, distanceMoyenne_input_case, marker="v", color="red")
+    ax1.grid(True)
+    # ax2 = fig1.add_subplot(212, xlabel='temps (s)', ylabel='vitesse (m/s)')
+    # ax2.plot(x, v_array)
+    # ax2.grid(True)
 
     graph = FigureCanvasTkAgg(fig, master=win)
     canvas = graph.get_tk_widget()
@@ -116,33 +123,34 @@ def accelerateur_lineaire():
 win = Tk()
 win.title("Acceleration constante le long d'un axe")
 
-vitesseIniVar = StringVar()
+vitesseTanSur_cVar = StringVar()
 rayonVar = StringVar()
 dureeViePropreVar = StringVar()
 
-vitesseIniValue = DoubleVar()
+vitesseTanSur_cValue = DoubleVar()
 rayonValue = DoubleVar()
 dureeViePropreValue = DoubleVar()
 
-vitesseIniValue.set(v0)
+vitesseTanSur_cValue.set(vitesseTanSur_c_input)
 rayonValue.set(rayon)
 dureeViePropreValue.set(dureeViePropre)
 
-vitesseIniVar.set("Vitesse initiale (en m/s)")
+vitesseTanSur_cVar.set("Rapport vitesse tangentielle sur c : v_t/c")
 rayonVar.set("Rayon (en m)")
 dureeViePropreVar.set("Durée de vie (en s)")
 
-vitesseIni_text = Label(win, text=vitesseIniVar.get())
+vitesseTanSur_c_text = Label(win, text=vitesseTanSur_cVar.get())
 rayon_text = Label(win, text=rayonVar.get())
 dureeViePropre_text = Label(win, text=dureeViePropreVar.get())
 
-vitesseIni_input = Entry(win, textvariable=vitesseIniValue)
+vitesseTanSur_c_input = Entry(win, textvariable=vitesseTanSur_cValue)
 rayon_input = Entry(win, textvariable=rayonValue)
 dureeViePropre_input = Entry(win, textvariable=dureeViePropreValue)
 
-calcul = Button(win, text='Calculer', command=accelerateur_lineaire)
+calcul = Button(win, text='Calculer', command=lancement_calcul)
 fig = Figure()
-ax = fig.add_subplot(111, xlabel='Vitesse tangentielle (m/s)', ylabel='Durée de vie du muon en laboratoire (s)')
+ax = fig.add_subplot(111, xlabel='Rapport vitesse tangentielle sur c : v_t/c ([0;1[)',
+                     ylabel='Durée de vie du muon en laboratoire (s)')
 ax.grid(True)
 
 fig1 = Figure()
@@ -157,10 +165,10 @@ canvas = graph.get_tk_widget()
 canvas2 = graph2.get_tk_widget()
 canvas.grid(row=0, column=0, pady=0, columnspan=5)
 canvas2.grid(row=0, column=6)
-vitesseIni_text.grid(row=1, column=0)
+vitesseTanSur_c_text.grid(row=1, column=0)
 rayon_text.grid(row=1, column=1)
 dureeViePropre_text.grid(row=1, column=2)
-vitesseIni_input.grid(row=2, column=0)
+vitesseTanSur_c_input.grid(row=2, column=0)
 rayon_input.grid(row=2, column=1)
 dureeViePropre_input.grid(row=2, column=2)
 calcul.grid(row=1, column=3, pady=0, rowspan=2, columnspan=2)
